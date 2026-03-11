81bda532d5136b93cc3a8f27e0b8c3b70ae60767
aquie00t <aquie00t@gmail.com>
2026-03-11 16:13:44 +0300

    chore: WorkExperience deleted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Projects from "../components/Projects";
-import WorkExperience from "../components/WorkExperience";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
@@ -13,7 +12,6 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
-      <WorkExperience />
       <Contact />
       <Footer />
     </div>
