32b865209a97697881b85389098a5fa560ca4449
unknown <aquie00t@icloud.com>
2026-05-03 23:23:53 +0300

    ci: fix tests

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  esbuild: {
+    jsx: "automatic",
+  },
   test: {
     environment: "jsdom",
     globals: true,
